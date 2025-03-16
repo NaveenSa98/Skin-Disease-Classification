e9574d94ee8d7b291bfb372c766b6f00e5a20b03
Naveen Samaranayake <140111923+NaveenSa98@users.noreply.github.com>
2025-03-17 00:36:51 +0530

    model training added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -18,4 +18,18 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: str
+
+
+@dataclass(frozen=True)
+class TrainingModelConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+    params_learning_rate: float
+
     
